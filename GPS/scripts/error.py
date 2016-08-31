705792234cebc519b20aae135c8f17ebd81e532a
George Almasi <ga@mailinator.com>
2016-08-31 12:44:12 -0400

    - ‘GPS/scripts/.#error.py’ / ‘GPS/scripts/error.py’

--- GPS/scripts/error.py
+++ GPS/scripts/error.py
@@ -14,4 +14,4 @@ def read_coords (fname):
             exit(1)
 
 
-read_coords ('/Users/galmasi/SparkleShare/FressMeinGrass/GPS/collected_datafiles/
+read_coords ('/Users/galmasi/SparkleShare/FressMeinGrass/GPS/collected_datafiles/083016/
