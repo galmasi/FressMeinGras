d5de92763aa074d4d2fcdb276c4bc480b61333f1
George Almasi <ga@mailinator.com>
2016-08-31 12:58:56 -0400

    / ‘GPS/scripts/error.py’

--- GPS/scripts/error.py
+++ GPS/scripts/error.py
@@ -19,7 +19,7 @@ def read_coords (fname):
                 result[timesig]=(lat,long)
         except Exception, e:
             print 'ERROR: %s'%(str(e))
-            print values[2],valuesp[4]
+            print values[2],values[4]
             exit(1)
     return timeline,result
 
