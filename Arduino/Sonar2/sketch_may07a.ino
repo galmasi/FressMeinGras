546fbbf52abae8838e94efc44cf3d5b433b783d7
George Almasi <nobody@nowhere.com>
2017-09-19 23:54:09 -0400

    --

---
+++ Arduino/Sonar2/sketch_may07a.ino
@@ -0,0 +1,86 @@
+#define trigPin1 13
+#define echoPin1 12
+#define trigPin2 11
+#define echoPin2 10
+#define trigPin3 9
+#define echoPin3 8
+#define trigPin4 7
+#define echoPin4 6
+#define trigPin5 5
+#define echoPin5 4
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(trigPin4, OUTPUT);
+  pinMode(echoPin4, INPUT);
+  pinMode(trigPin5, OUTPUT);
+  pinMode(echoPin5, INPUT);
+}
+
+void loop() {
+  long duration;
+  float distance1, distance2, distance3, distance4, distance5;
+
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  duration = pulseIn(echoPin1, HIGH);
+  distance1 = (duration/2) / 29.1;
+  delay(50);
+
+  digitalWrite(trigPin3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin3, LOW);
+  duration = pulseIn(echoPin3, HIGH);
+  distance3 = (duration/2) / 29.1;
+  delay(50);
+
+  digitalWrite(trigPin5, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin5, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin5, LOW);
+  duration = pulseIn(echoPin5, HIGH);
+  distance5 = (duration/2) / 29.1;
+  delay(50);
+
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  duration = pulseIn(echoPin2, HIGH);
+  distance2 = (duration/2) / 29.1;
+  delay(50);
+  
+
+  digitalWrite(trigPin4, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin4, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin4, LOW);
+  duration = pulseIn(echoPin4, HIGH);
+  distance4 = (duration/2) / 29.1;
+  delay(50);
+  Serial.print(distance1);
+  Serial.print(" ");
+  Serial.print(distance2);
+  Serial.print(" ");
+  Serial.print(distance3);
+  Serial.print(" ");
+  Serial.print(distance4);
+  Serial.print(" ");
+  Serial.println(distance5);
+  
+
+} 
