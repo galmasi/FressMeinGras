bea878410b76597298e52fb726d150bd91668e35
almasig <almasig@osctrl.watson.ibm.com>
2017-09-28 09:50:04 -0400

    ultrasoundradar now uses compass data

--- GUI/ultrasoundradar2.py
+++ GUI/ultrasoundradar2.py
@@ -4,30 +4,42 @@ import Tkinter
 import serial
 import math
 
-cos_list=[]
-sine_list=[]
-
-for i in range(-2,3):
-    cos_list.append(math.cos(math.pi*i/5))
-    sine_list.append(math.sin(math.pi*i/5))
-    
-print cos_list         
-
-
 def redraw_canvas (ser, w, taglist):
+    # canvas dimensions
     global canvas_height
-    global radius
+    global canvas_width
+
+    # the canvas radius of the circle on which we plot (pixels)
+    global canvas_radius
+
     l = ser.readline()
     values=l.split()
 
     for i in range(0,5):
-        dist=float(values[i])*radius/400.0
-        x0=dist*cos_list[i]-5
-        y0=canvas_height/2-dist*sine_list[i]-5
-        x1=dist*cos_list[i]+5
-        y1=canvas_height/2-dist*sine_list[i]+5
-        w.coords(taglist[i],x0,y0,x1,y1)
-    
+
+        # center point
+        centerx=canvas_width/2
+        centery=canvas_height/2
+
+        # local angle in radians
+        angle = (i-2)*math.pi/5 + float(values[0])*math.pi/180.0
+
+        # direction vector endpoint
+        
+        dirx = centerx + canvas_radius * math.cos(angle)
+        diry = centery + canvas_radius * math.sin(angle)
+
+        # local distance in units relative to radius
+        distance = float(values[i+1])/400.0 * canvas_radius
+        
+        # distance endpoint
+        distx = centerx + distance * math.cos(angle)
+        disty = centerx + distance * math.sin(angle)
+
+        # adjust tags 
+        w.coords(taglist[i],centerx,centery,dirx,diry)
+        w.coords(taglist[i+5],distx-5,disty-5,distx+5,disty+5)
+        
     w.update()
     w.update_idletasks()
     w.after(100, redraw_canvas, ser, w, taglist)
@@ -40,28 +52,21 @@ ser = serial.Serial('/dev/ttyUSB0')
 root = Tkinter.Tk()
 canvas_width=400
 canvas_height=400
-radius=200
+canvas_radius=200
+
 w = Tkinter.Canvas(root, width=canvas_width, height=canvas_height, bg='gray')
 w.pack(fill=Tkinter.BOTH)
 
-
+# create canvas objects with dummy coordinates
 taglist=[]
-for i in range(0,5):
-    x0=0
-    y0=canvas_height/2
-    x1=x0+radius*cos_list[i]
-    y1=y0+radius*sine_list[i]
-    taglist.append(w.create_line(x0,y0,x1,y1))
+centerx=canvas_width/2
+centery=canvas_height/2    
 
-taglist=[]
 for i in range(0,5):
-    x0=radius*cos_list[i]-5
-    y0=canvas_height/2+radius*sine_list[i]-5
-    x1=radius*cos_list[i]+5
-    y1=canvas_height/2+radius*sine_list[i]+5
-    tag=w.create_oval(x0,y0,x1,y1,fill='red')
-    taglist.append(tag)
-#    print w.coords(tag)
+    taglist.append(w.create_line(centerx,centery,centerx+1,centery+1))
+
+for i in range(0,5):
+    taglist.append(w.create_oval(centerx,centery,centerx+1,centery+1,fill='red'))
 
 w.after_idle(redraw_canvas, ser, w, taglist)
 
